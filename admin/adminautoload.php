98d493c8e9c83e34bbb8c02ed85cae01665dec7b
John Good <judiconis@gmail.com>
2012-06-10 19:21:01 +0100

    Stuff

--- admin/adminautoload.php
+++ admin/adminautoload.php
@@ -1,5 +1,5 @@
 <?php
-	require(dirname(__FILE__) . "/../lib/autoload.php");
+	require_once(dirname(__FILE__) . "/../lib/autoload.php");
 	if ($_SESSION['user']['role'] != "ADMIN") {
 		header('location:http://arachnias.co.cc/lg/');
 	}
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,3 @@
-<?php require_once('adminautoload.php');?>
 <!DOCTYPE html>
 <html>
 <head>
