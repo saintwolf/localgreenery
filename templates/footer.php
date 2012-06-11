5494d0d74e118a2dadaa48319c9b2576f55ca38c
John Good <judiconis@gmail.com>
2012-06-11 17:27:47 +0100

    Added Help button to footer (Not implemented yet)

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <br />
 <br />
 <footer>
-	<a href="/index.php">Home</a><?php if($_SESSION['user']['role'] == "ADMIN") echo ' - <a href="/admin/">Admin Menu</a>'; ?>
+	<a href="/index.php">Home</a><?php if($_SESSION['user']['role'] == "ADMIN") echo ' - <a href="/admin/">Admin Menu</a>'; ?> - <a href="">Help</a>
 	<br />Powered by LocalGreenery. <br />
 	&copy; 2012 Mash-Heads.
 </footer>
