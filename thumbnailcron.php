cce532beee3c2d790fdd6f91b69cac26e4f0fa8c
Saintwolf <judiconis@gmail.com>
2012-06-11 23:28:55 +0000

    Now skips files that are already processed in thumbnailcron

--- thumbnailcron.php
+++ thumbnailcron.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 define('DS', DIRECTORY_SEPARATOR);
 require_once(__DIR__ . DS . 'lib' . DS . 'functions.php');
 $uploadDir = __DIR__ . DS . 'uploads';
@@ -11,9 +11,11 @@ if ($pics[0]!='')
 {
 	foreach ($pics as $p)
 	{
-		if ($p != '' && $p != '.' && $p !=  '..' && $p !=  'thumbs') {
-		echo "Create thumb for $p\n";
-		createthumb($uploadDir . DS . $p, $uploadDir . DS . 'thumbs' . DS . 'tn_'.$p,100,100);
+		if (file_exists($uploadDir . DS . 'thumbs' . DS . 'tn_'.$p)) {
+			if ($p != '' && $p != '.' && $p !=  '..' && $p !=  'thumbs') {
+				echo "Create thumb for $p\n";
+				createthumb($uploadDir . DS . $p, $uploadDir . DS . 'thumbs' . DS . 'tn_'.$p,100,100);
+			}
 		}
 	}
 	echo 'Thumbs have been created!';
