89cf559e3d081177ecc4c9db0faa46bbace9f112
John Good <judiconis@gmail.com>
2012-06-11 22:29:21 +0100

    Made logincron work from outside the working directory

--- thumbnailcron.php
+++ thumbnailcron.php
@@ -1,5 +1,6 @@
 <?php
-require_once('./lib/functions.php');
+define('DS', DIRECTORY_SEPARATOR);
+require_once(__DIR__ . DS . 'lib' . DS . 'functions.php');
 $dir = opendir('./uploads');
 //$pics=dirname();
 while ($pic = readdir($dir)) {
@@ -10,8 +11,9 @@ if ($pics[0]!='')
 	foreach ($pics as $p)
 	{
 		if ($p != '' && $p != '.' && $p !=  '..' && $p !=  'thumbs') {
-		echo "Create thumb for $p";
+		echo "Create thumb for $p\n";
 		createthumb('uploads/'.$p,'uploads/thumbs/tn_'.$p,100,100);
 		}
 	}
+	echo 'Thumbs have been created!';
 }
