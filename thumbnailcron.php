bef20722f389877a5fe9cdedbbe693b44da997a3
John Good <judiconis@gmail.com>
2012-06-12 09:55:10 +0100

    thumbnailcron now checks if the upload and thumbnail directories exist, and creates them if they don't.

--- thumbnailcron.php
+++ thumbnailcron.php
@@ -2,6 +2,15 @@
 define('DS', DIRECTORY_SEPARATOR);
 require_once(__DIR__ . DS . 'lib' . DS . 'functions.php');
 $uploadDir = __DIR__ . DS . 'uploads';
+// Check if the upload and thumbs directory exist
+if (!file_exists($uploadDir)) {
+    echo 'Creating upload directory at: ' . $uploadDir . "\n";
+    mkdir($uploadDir, 0755) or die('Fatal: Unable to create directory: ' . $uploadDir . "\n");
+}
+if (!file_exists($uploadDir . DS . 'thumbs')) {
+    echo 'Creating thumbs directory at: ' . $uploadDir . DS . 'thumbs' . "\n";
+    mkdir($uploadDir . DS . 'thumbs', 0755) or die('Fatal: Unable to create directory: ' . $uploadDir . "\n");
+}
 $dir = opendir($uploadDir);
 //$pics=dirname();
 while ($pic = readdir($dir)) {
