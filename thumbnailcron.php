9a936c8278541f4708c2179510a4f3aaa166e1ae
John Good <judiconis@gmail.com>
2012-06-12 09:17:08 +0100

    thumbnailcron.php will no longer generate thumbs that already exist.

--- thumbnailcron.php
+++ thumbnailcron.php
@@ -5,16 +5,26 @@ $uploadDir = __DIR__ . DS . 'uploads';
 $dir = opendir($uploadDir);
 //$pics=dirname();
 while ($pic = readdir($dir)) {
-	$pics[] = $pic;
+    $pics[] = $pic;
 }
-if ($pics[0]!='')
-{
-	foreach ($pics as $p)
-	{
-		if ($p != '' && $p != '.' && $p !=  '..' && $p !=  'thumbs') {
-		echo "Create thumb for $p\n";
-		createthumb($uploadDir . DS . $p, $uploadDir . DS . 'thumbs' . DS . 'tn_'.$p,100,100);
-		}
-	}
-	echo 'Thumbs have been created!';
+if ($pics[0] != '') {
+    foreach ($pics as $p) {
+
+        if ($p != '' && $p != '.' && $p != '..' && $p != 'thumbs') {
+            if (!file_exists($uploadDir . DS . 'thumbs' . DS . 'tn_' . $p)) {
+                echo "Create thumb for " . $uploadDir . DS . $p . " in "
+                        . $uploadDir . DS . 'thumbs' . DS . 'tn_' . $p . "\n";
+                createthumb($uploadDir . DS . $p,
+                        $uploadDir . DS . 'thumbs' . DS . 'tn_' . $p, 100, 100);
+            } else {
+                echo "Skipping: Thumbnail already generated: " . $uploadDir
+                        . DS . 'thumbs' . DS . 'tn_' . $p . "\n";
+            }
+        } else {
+            echo "Skipping: Directory: " . $uploadDir . DS . $p
+                    . "\n";
+        }
+
+    }
+    echo 'Thumbs have been created!';
 }
