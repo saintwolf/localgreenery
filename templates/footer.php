cf7302915ad8007a48d20b6087558aee63c66263
John Nerush <john@nerush.co.uk>
2014-02-22 19:21:32 +0000

    Initial commit. Remove some older files, strip CSS and install Bootstrap v3/jQuery v1.11.

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,10 @@
-<footer>
-	<a href="/index.php">Home</a><?php if($session->user['role'] == "ADMIN") echo ' - <a href="/admin/">Admin Menu</a>'; ?> - <a href="">Help</a>
-	<br />Powered by LocalGreenery. <br />
-	&copy; 2012 Mash-Heads.
-</footer>
-</div>
-</body>
+
+	<a href="/index.php">Home</a><?php if($session->user['role'] == "ADMIN") echo ' - <a href="/admin/">Admin Menu</a>'; ?>
+	<br />Powered by LocalGreenery &copy; 2012 - <?php echo date('Y'); ?> Mash-Heads.
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LocalGreenery</title>
 
-<link rel="stylesheet" type="text/css" href="/css/css.css" />
-<link rel="apple-touch-icon" href="/img/Black_Widow.png" />
-<meta name="viewport" content="width=device-width; initial-scale = 1.0; maximum-scale=1.0; user-scalable=no" />
-<title>LocalGreenery</title>
-</head>
-<body class="mainbody">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
 <div class="appcontainer">
 Seller is <strong><?php echo $options['status']['value']; ?></strong><br />
 You are logged in as <?php echo $session->user['username']; ?><br />
